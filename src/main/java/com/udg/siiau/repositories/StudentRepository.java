7e00284655ba66353efd3b968e9bb38002ab3aa9
Brayitaan <brycasale@gmail.com>
2024-04-23 17:27:13 -0600

    first commit

---
+++ src/main/java/com/udg/siiau/repositories/StudentRepository.java
@@ -0,0 +1,13 @@
+package com.udg.siiau.repositories;
+
+import com.udg.siiau.models.StudentModel;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface StudentRepository  extends CrudRepository<StudentModel, Long> {
+    public Optional<StudentModel> findByCode(String code);
+    public Optional<StudentModel> findByName(String name);
+}
